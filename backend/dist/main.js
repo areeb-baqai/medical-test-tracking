1859500aa60d21d600f4f1450692f71a8af2e653
Areeb <areebbaqai@gmail.com>
2025-03-13 11:13:23 +0500

    feature fix issue

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const cookieParser = require("cookie-parser");
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
@@ -23,7 +26,7 @@ async function bootstrap() {
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         allowedHeaders: "Content-Type,Accept,Authorization",
     });
-    app.use(cookieParser());
+    app.use((0, cookie_parser_1.default)());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
